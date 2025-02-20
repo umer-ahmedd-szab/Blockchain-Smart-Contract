90d8efff966cc375f2d5ced8986704b91c961ccf
umer-ahmedd-szab <bcsbs2112337@szabist.pk>
2025-02-20 13:08:21 +0500

    initial commit

---
+++ contracts/lecture1.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract lecture1
+{
+    
+    // function concat(string memory a, string memory b) public pure returns (string memory){
+    //     return string.concat(a," ",b);
+    // }
+
+    // function checkEven(int num) public pure returns (string memory){
+    //     if (num % 2 == 0){
+    //         return "Number is even";
+    //     } else {
+    //         return "Number is odd";
+    //     }
+    // }
+
+    // bool val = true;
+
+    // function boolCheck() public returns (bool){
+    //     if (val == true){
+    //         val = false;
+    //     } else {
+    //         val = true;
+    //     }
+    //     return val;
+    // }
+
+    // int public i = 0;
+
+    // function inc() public returns (int){
+    //     return i++;
+    // }
+
+    // function dec() public returns (int) {
+    //     return i--;
+    // }
+
+    // string private message = "Hello World";
+
+    // function add(int a, int b) public pure returns (int) {
+    //     return a + b;
+    // }
+
+    // function sub(int a, int b) public pure returns (int) {
+    //     return a - b;
+    // }
+
+    // function multiply(int a, int b) public pure returns (int) {
+    //     return a * b;
+    // }
+
+    // function divide(int a, int b) public pure returns (int) {
+    //     return a / b;
+    // }
+
+    // function returnMessage() public view returns (string memory) {
+    //     return message;
+    // }
+}
